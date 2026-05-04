c92d08b68d87fbdd116b6dcc5dbe7b7ed4e8b2d5
Fábio M. Costa <fmc@ufg.br>
2026-05-04 13:36:54 -0300

    Update tasksrc.py

--- pipeline_producer-consumer/tasksrc.py
+++ pipeline_producer-consumer/tasksrc.py
@@ -2,16 +2,17 @@ import multiprocessing
 import zmq, time, pickle, sys, random
 from constPipe import * #-
 
-def producer(port):
+def producer(id, port):
 	context = zmq.Context()              
 	socket  = context.socket(zmq.PUSH)        # create a push socket
 	socket.bind("tcp://localhost:"+port)                             # bind socket to address
 	
 	for i in range(100):                  # generate 100 workloads
 		workload = random.randint(1, 100)   # compute workload
+		print ("Producer " + id + " sent workload item of size " + str(workload))
 		socket.send(pickle.dumps(workload)) # send workload to worker
 
 if sys.argv[1] == '1':
-	producer(PORT1)
+	producer('1', PORT1)
 elif sys.argv[1] == '2':
-	producer(PORT2)
+	producer('2', PORT2)
