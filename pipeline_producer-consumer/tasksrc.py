28e31f085efba2872fab7e241d4224831a663cf8
Fábio M. Costa <fmc@ufg.br>
2026-05-04 13:37:11 -0300

    Fix print statement for workload sending

--- pipeline_producer-consumer/tasksrc.py
+++ pipeline_producer-consumer/tasksrc.py
@@ -9,7 +9,7 @@ def producer(id, port):
 	
 	for i in range(100):                  # generate 100 workloads
 		workload = random.randint(1, 100)   # compute workload
-		print ("Producer " + id + " sent workload item of size " + str(workload))
+		print ("Producer " + id + " sending workload item of size " + str(workload))
 		socket.send(pickle.dumps(workload)) # send workload to worker
 
 if sys.argv[1] == '1':
